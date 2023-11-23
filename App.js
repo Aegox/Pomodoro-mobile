2eb0ad6c57c90c44c3dd26f3cdba173dd77ae124
Diego Amundaray <diegoamundaray2017@gmail.com>
2023-11-22 22:12:25 -0500

    add structure

--- App.js
+++ App.js
@@ -4,8 +4,16 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>The Pomodoro Clock</Text>
       <StatusBar style="auto" />
+      <View style={styles.clock}>
+        <View style={styles.time}>
+          <Text style={styles.text}>30</Text>
+        </View>
+        <View style={styles.time}>
+          <Text style={styles.text}>00</Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +21,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#060606',
+},
+  title: {
+    color: 'white',
+    alignSelf: 'center',
+    fontSize: 35,
+    marginTop: 20
+  },
+  clock: {
+    flex: 1,
+    width: '100%',
+    height: '60%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: '1%'
+  },
+  time: {
+    height: '45%',
+    width: '50%',
+    backgroundColor: '#222',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: 'white',
+    fontSize: 150,
+    fontFamily: 'BebasNeueBold'
+    }
 });
